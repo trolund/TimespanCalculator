934820f7495a5e3da9a2c3caa321fc649b3c0c7b
Troels Lund <trolund@gmail.com>
2021-05-06 16:57:28 +0200

    pwa works

--- next.config.js
+++ next.config.js
@@ -1,31 +1,5 @@
-// Progressive Web App: Add service worker with network-first strategy.
-// Network-first strategy means that if there is no internet connection,
-// the browser will use files previously saved locally to the user’s device instead.
-// AKA Offline Mode!
-const withOffline = require("next-offline");
+const withPWA = require('next-pwa')
 
-const nextConfig = {
-  target: "serverless",
-  workboxOpts: {
-    swDest: "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "networkFirst",
-        options: {
-          cacheName: "https-calls",
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
-  }
-};
+const nextConfig = withPWA({})
 
-module.exports = withOffline(nextConfig);
+module.exports = nextConfig

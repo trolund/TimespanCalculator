92512b8266773c7cfda8fa9c355db2762056f35c
Troels Lund <trolund@gmail.com>
2021-05-07 22:37:16 +0200

    black-translucent

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -53,73 +53,73 @@ export default class MyDocument extends Document {
 
           {/* Bonus: Have app icon and splash screen for PWAs saved to homescreen on iOS devices */}
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-            <link
-              rel="apple-touch-icon"
-              sizes="57x57"
-              href="./static/graphics/icon-57.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="72x72"
-              href="./static/graphics/icon-72.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="114x114"
-              href="./static/graphics/icon-114.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="144x144"
-              href="./static/graphics/icon-144.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="512x512"
-              href="./static/graphics/icon-512.png"
-            />
-            <link
-              href="./static/graphics/splash-2048.png"
-              media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
-              rel="apple-touch-startup-image"
-            />
-            <link
-              href="./static/graphics/splash-1668.png"
-              media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
-              rel="apple-touch-startup-image"
-            />
-            <link
-              href="./static/graphics/splash-1536.png"
-              media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
-              rel="apple-touch-startup-image"
-            />
-            <link
-              href="./static/graphics/splash-1125.png"
-              media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-              rel="apple-touch-startup-image"
-            />
-            <link
-              href="./static/graphics/splash-1242.png"
-              media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
-              rel="apple-touch-startup-image"
-            />
-            <link
-              href="./static/graphics/splash-750.png"
-              media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-              rel="apple-touch-startup-image"
-            />
-            <link
-              href="./static/graphics/splash-640.png"
-              media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-              rel="apple-touch-startup-image"
-            />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="./static/graphics/icon-57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="./static/graphics/icon-72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="./static/graphics/icon-114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="./static/graphics/icon-144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="512x512"
+            href="./static/graphics/icon-512.png"
+          />
+          <link
+            href="./static/graphics/splash-2048.png"
+            media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-1668.png"
+            media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-1536.png"
+            media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-1125.png"
+            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-1242.png"
+            media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-750.png"
+            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
+          <link
+            href="./static/graphics/splash-640.png"
+            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+            rel="apple-touch-startup-image"
+          />
         </Head>
-          <body>
-            <Main />
-            <NextScript />
+        <body>
+          <Main />
+          <NextScript />
 
-          </body>
+        </body>
       </html>
     );
   }
@@ -156,14 +156,14 @@ MyDocument.getInitialProps = async (ctx: DocumentContext) => {
 
   ctx.renderPage = () =>
     originalRenderPage({
-          enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
 
   const initialProps = await Document.getInitialProps(ctx);
 
   return {
-          ...initialProps,
-          // Styles fragment is rendered after the app and page rendering finish.
-          styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
   };
 };
